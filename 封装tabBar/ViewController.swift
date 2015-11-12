7ceb6371375a398f9adc4750063e89a9d9ed1f3d
Iceing <1948049031@qq.com>
2015-11-12 11:02:37 +0800

    Initial Commit

---
+++ 封装tabBar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  封装tabBar
+//
+//  Created by 蔡跃明 on 15/11/12.
+//  Copyright © 2015年 lm. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
